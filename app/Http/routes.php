09f5531f8b82ab86762d9508ba01b823efbff4f0
eldario666 <eldario666@gmail.com>
2018-10-24 23:55:36 +0500

    Skeleton
    Skeleton
    users, forgot migrate
    first part of localizations

---
+++ app/Http/routes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 24.10.2018
+ * Time: 22:39
+ */
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Маршруты регистрации...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
